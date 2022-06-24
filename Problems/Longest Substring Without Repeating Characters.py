38a969d484f44c14d81a7eb52ae4fd012fc2cc1f
João Paulo Machado <joaopaulomachado@MacBook-Pro-de-Joao.local>
2022-06-23 22:36:55 -0300

    Longest Substring Without Repeating Characters

---
+++ Problems/Longest Substring Without Repeating Characters.py
@@ -0,0 +1,41 @@
+from locale import currency
+
+#Using Index
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = 0
+        currencySubstring = ""
+
+
+        for char in s:
+            if(char in currencySubstring):
+                currencySubstring = currencySubstring[currencySubstring.index(char)+1:]
+            
+            currencySubstring+= char
+
+            longest = max(longest, len(currencySubstring) )
+        return longest
+
+
+# Using Set
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         longest = 0
+#         currencySubstring = set()
+#         l = 0
+
+
+#         for char in s:
+#             while(char in currencySubstring):
+#                 currencySubstring.remove(s[l])
+#                 l+=1
+            
+#             currencySubstring.add(char)
+            
+
+#             longest = max(longest, len(currencySubstring) )
+#         return longest
+
+s = "pwwkew"
+obj = Solution()
+print(obj.lengthOfLongestSubstring(s))
