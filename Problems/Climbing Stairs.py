da27b7f89ef916cc5c82a9a569f39f6d91bb322d
João Paulo Machado <joaopaulomachado@MacBook-Pro-de-Joao.local>
2022-06-26 13:54:15 -0300

    Climbing Stairs

---
+++ Problems/Climbing Stairs.py
@@ -0,0 +1,18 @@
+from functools import cache
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = { 1:1, 2:2}
+
+        def climStairstCustom( stemps):
+            if(stemps in cache):
+                return cache[stemps]
+            
+            cache[stemps] = climStairstCustom(stemps-1) +climStairstCustom(stemps-2) 
+            return cache[stemps]
+        
+        return climStairstCustom(n) 
+
+obj = Solution()
+print(obj.climbStairs(5))
