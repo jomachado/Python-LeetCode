82558647f329c9257dc6614f34ad8fb73a89db1f
João Paulo Machado <joaopaulomachado@MacBook-Pro-de-Joao.local>
2022-06-28 21:41:57 -0300

    Majority Element

---
+++ Problems/Majority Element.py
@@ -0,0 +1,26 @@
+from typing import List
+import math
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        majorThenSize = math.floor(len(nums)/2)
+        values = dict()
+
+        for item in nums:
+            if(not item in values):
+                values[item] = 0
+
+            values[item] += 1
+
+            if  values[item] > majorThenSize:
+                return  values[item]
+            
+
+nums = [3,2,3]
+obj = Solution()
+
+print(obj.majorityElement(nums))
+
+
+
+        
