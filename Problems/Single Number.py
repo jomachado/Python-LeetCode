7e8ae29f4a786415d3e1daf6182737949f7fbf2d
João Paulo Machado <joaopaulomachado@MacBook-Pro-de-Joao.local>
2022-07-10 13:40:05 -0300

    Single Number

---
+++ Problems/Single Number.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution: 
+    def singleNumber(self, nums: List[int]) -> int:
+            number = 0
+            for item in nums:
+                number = number ^ item
+            return number
+
+obj = Solution()
+print(obj.singleNumber([1,3]))
