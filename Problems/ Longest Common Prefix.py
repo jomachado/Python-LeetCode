9813829d2582217897c9398b186bf38ff22683d2
João Paulo Machado <joaopaulomachado@MacBook-Pro-de-Joao.local>
2022-07-10 13:51:02 -0300

    Two files

---
+++ Problems/ Longest Common Prefix.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        position = 0
+        commontPrefix = ""
+        for i in strs[0]:
+            for y in range(0, len(strs), 1):
+                if(position >= len(strs[y]) or strs[y][position] != i ):
+                    return commontPrefix                
+            
+            commontPrefix += i
+            position+=1
+
+        return commontPrefix
+
+obj = Solution()        
+strs = ["ab","a"]    
+print(obj.longestCommonPrefix(strs))
+        
+        
+
---
+++ Problems/Implement strStr().py
@@ -0,0 +1,25 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle: 
+            return 0
+                     
+        l = 0 
+        r = len(needle)-1
+        while(r <= len(haystack)):
+            checkNeedle = haystack[l:r+1]
+
+            if(checkNeedle == needle):
+                return l
+            
+            l += 1
+            r += 1
+
+        return -1
+
+            
+
+        
+obj = Solution()
+haystack = "abc"
+needle = "c"
+print(obj.strStr(haystack, needle))
