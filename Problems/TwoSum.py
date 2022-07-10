cbe03e9dea2446dd404a420b474e51638539e768
João Paulo Machado <joaopaulomachado@MacBook-Pro-de-Joao.local>
2022-07-10 13:51:50 -0300

    Two Sum

--- Problems/TwoSum.py
+++ Problems/TwoSum.py
@@ -3,6 +3,7 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numsCombinations = dict()
+
         for i in range(len(nums)):
             sub = target - nums[i]
             if(sub in numsCombinations):
